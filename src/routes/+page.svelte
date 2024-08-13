73b452feff3cfa2dacc78c6887574b076da2b7a2
n1ved <nivedrsalini@gmail.com>
2024-08-13 18:13:39 +0530

    Added Home

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+
+</script>
+
+<div>
+    <h1 class="name">Nived</h1>
+
+    <p class="home_desc">
+        Hi I’m Nived , Third year <span>Computer Science Engineering</span> student at <span>Govt. Model Engineering College</span>
+
+        <span class="sep"></span>
+
+        I like to work with <span>Front End</span> and <span>Mobile App Development</span> and currently learning other programming technologies
+    </p>
+</div>
+
+<style>
+    div{
+        max-width: 75vw;
+        height: 100vh;
+        box-sizing: border-box;
+        padding: 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+    }
+    .name{
+        font-size: 200px;
+    }
+    .sep{
+        display: block;
+        width: 100%;
+        height: 2px;
+        margin: 5px 0;
+    }
+    .home_desc{
+        width: 70%;
+        font-size: 24px;
+        font-family: ClashDisplay-Light,'serif';
+        font-weight: 300;
+    }
+    span{
+        font-weight: 600;
+    }
+</style>
