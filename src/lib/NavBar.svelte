dba4def115ec86dc6a5ce0bc19e5099f1cf1a9e8
n1ved <nivedrsalini@gmail.com>
2024-08-13 17:56:38 +0530

    Add Navbar

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import './NavBar.css'
+</script>
+<div class="navbar">
+    <div class="links">
+        <a href="/" >Home</a>
+        <a href="/about" >About</a>
+        <a href="/projects" >Projects</a>
+        <a href="#">Resume</a>
+    </div>
+    <div class="socials">
+        <div>
+            <h2>Socials</h2>
+        </div>
+        <div>
+            <a href="#>">Github</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">Mastodon</a>
+            <a href="#">Telegram</a>
+            <a href="#">Email</a>
+        </div>
+    </div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Navbar from "$lib/NavBar.svelte";
+    import '../app.css'
+    import '../fonts/clash-display/css/clash-display.css'
+</script>
+
+<Navbar />
+<slot/>
