9dc5db64c7afff7d94accaa558a135b54999cc7d
n1ved <nivedrsalini@gmail.com>
2024-08-24 18:59:45 +0530

    (responsive/navbar) create responsive navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,23 +1,47 @@
 <script>
     import './NavBar.css'
+
+    function sleep() {
+        return new Promise(resolve => setTimeout(resolve, 600));
+    }
+
+    function toggleMenu() {
+        const menu = document.querySelector('.navbar')
+        menu?.classList.toggle('active')
+    }
+    
+    function toggleMenuOnLinks(){
+        const menu = document.querySelector('.navbar')
+
+        if(menu?.classList.contains('active')){
+            sleep().then(() => {
+                menu?.classList.remove('active')
+            })
+        }
+    }
 </script>
+<div class="navbar-hamburger">
+    <button class="menu-btn" on:click={toggleMenu}>
+        MENU
+    </button>
+</div>
 <div class="navbar">
     <div class="links">
-        <a href="/" >Home</a>
-        <a href="/about" >About</a>
-        <a href="/projects" >Projects</a>
-        <a href="/docs/resume.pdf" target="_blank">Resume</a>
+        <a href="/" on:click={toggleMenuOnLinks}>Home</a>
+        <a href="/about" on:click={toggleMenuOnLinks}>About</a>
+        <a href="/projects" on:click={toggleMenuOnLinks}>Projects</a>
+        <a href="/docs/resume.pdf" target="_blank" on:click={toggleMenuOnLinks}>Resume</a>
     </div>
     <div class="socials">
         <div>
             <h2>Socials</h2>
         </div>
         <div>
-            <a href="https://github.com/n1ved/" target="_blank">Github</a>
-            <a href="https://linkedin.com/in/n1ved" target="_blank">LinkedIn</a>
-            <a href="https://fosstodon.org/@n1ved/" target="_blank">Mastodon</a>
-            <a href="https://t.me/n1vxd" target="_blank">Telegram</a>
-            <a href="mailto:n1ved@proton.me" target="_blank">Email</a>
+            <a href="https://github.com/n1ved/" target="_blank" on:click={toggleMenuOnLinks}>Github</a>
+            <a href="https://linkedin.com/in/n1ved" target="_blank" on:click={toggleMenuOnLinks}>LinkedIn</a>
+            <a href="https://fosstodon.org/@n1ved/" target="_blank" on:click={toggleMenuOnLinks}>Mastodon</a>
+            <a href="https://t.me/n1vxd" target="_blank" on:click={toggleMenuOnLinks}>Telegram</a>
+            <a href="mailto:n1ved@proton.me" target="_blank" on:click={toggleMenuOnLinks}>Email</a>
         </div>
     </div>
 </div>
