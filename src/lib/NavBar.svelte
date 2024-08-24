4c51ef4d9c3721f48db3d64dbe2b71401d0b762f
n1ved <nivedrsalini@gmail.com>
2024-08-24 13:24:22 +0530

    (content) navbar links added

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -6,18 +6,18 @@
         <a href="/" >Home</a>
         <a href="/about" >About</a>
         <a href="/projects" >Projects</a>
-        <a href="#">Resume</a>
+        <a href="/docs/resume.pdf" target="_blank">Resume</a>
     </div>
     <div class="socials">
         <div>
             <h2>Socials</h2>
         </div>
         <div>
-            <a href="#>">Github</a>
-            <a href="#">LinkedIn</a>
-            <a href="#">Mastodon</a>
-            <a href="#">Telegram</a>
-            <a href="#">Email</a>
+            <a href="https://github.com/n1ved/">Github</a>
+            <a href="https://linkedin.com/in/n1ved">LinkedIn</a>
+            <a href="https://fosstodon.org/@n1ved/">Mastodon</a>
+            <a href="https://t.me/n1vxd">Telegram</a>
+            <a href="mailto:n1ved@proton.me">Email</a>
         </div>
     </div>
 </div>
