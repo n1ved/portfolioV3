03c3f0dd9d1319beb217af626c08ceb4b459c4a3
n1ved <nivedrsalini@gmail.com>
2024-08-24 17:23:45 +0530

    (fix) target blank on social links

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -13,11 +13,11 @@
             <h2>Socials</h2>
         </div>
         <div>
-            <a href="https://github.com/n1ved/">Github</a>
-            <a href="https://linkedin.com/in/n1ved">LinkedIn</a>
-            <a href="https://fosstodon.org/@n1ved/">Mastodon</a>
-            <a href="https://t.me/n1vxd">Telegram</a>
-            <a href="mailto:n1ved@proton.me">Email</a>
+            <a href="https://github.com/n1ved/" target="_blank">Github</a>
+            <a href="https://linkedin.com/in/n1ved" target="_blank">LinkedIn</a>
+            <a href="https://fosstodon.org/@n1ved/" target="_blank">Mastodon</a>
+            <a href="https://t.me/n1vxd" target="_blank">Telegram</a>
+            <a href="mailto:n1ved@proton.me" target="_blank">Email</a>
         </div>
     </div>
 </div>
